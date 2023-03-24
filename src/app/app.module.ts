1dc8a4026b85970b57cbe549beb8f61ca93b0c25
Daneil <daneilhyman12@gmail.com>
2023-03-24 02:54:08 -0500

    this is a first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './partials/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
